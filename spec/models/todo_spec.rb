b95d6a17273c2fab8690a4031f429725b58cbe47
thaivankhoa <thaivankhoa2802@gmail.com>
2018-09-10 14:55:03 +0700

    model test

---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Todo, type: :model do
+  describe 'Association' do  
+    it { should have_many(:items).dependent(:destroy) } 
+  end
+  
+  describe 'Validation' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:created_by) }
+  end 
+end
